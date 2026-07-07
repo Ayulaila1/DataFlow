15fe46226fb909f83c23615f335ddb5cfd04062f
Ayu Fitria <fitrimut0711@gmail.com>
2026-07-07 10:47:08 +0700

    layout

---
+++ resources/views/livewire/auth/login-component.blade.php
@@ -0,0 +1,47 @@
+<div class="container">
+
+    <div class="row vh-100 justify-content-center align-items-center">
+
+        <div class="col-md-4">
+
+            <div class="card shadow">
+
+                <div class="card-body">
+
+                    <h2 class="text-center mb-4">
+
+                        DATAFLOW
+
+                    </h2>
+
+                    <div class="mb-3">
+
+                        <label>Email</label>
+
+                        <input type="email" class="form-control">
+
+                    </div>
+
+                    <div class="mb-3">
+
+                        <label>Password</label>
+
+                        <input type="password" class="form-control">
+
+                    </div>
+
+                    <button class="btn btn-primary w-100">
+
+                        Login
+
+                    </button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
